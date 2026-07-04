0f18e9e2d1778034c7907c02ddafb40fdbbe231c
mak4444 <fpauk@mail.ru>
2026-07-04 12:42:52 +0300

    Update types.h

--- lib/types.h
+++ lib/types.h
@@ -9,6 +9,7 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long uint64_t;
+typedef unsigned __int128 uint128_t;
 
 typedef signed char int8_t;
 typedef signed short int16_t;
